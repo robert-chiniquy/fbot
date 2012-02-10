39cd042981eabf4ffb84d3f708554e62a5032d55
Robert Chiniquy <rchiniquy@yahoo.com>
2012-02-09 18:19:19 -0800

    rch - has actual code now, does nothing

---
+++ README.md
@@ -0,0 +1,2 @@
+
+A bot to solve the problem of "I want to kick off this automation from IRC and view the stdout / stderr on a webpage"
---
+++ fbot.js
@@ -0,0 +1,8 @@
+
+exports.bot = require('lib/bot.js');
+exports.web = require('lib/web.js');
+exports.util = require('lib/util.js');
+
+exports.config = require('lib/config.js');
+
+
